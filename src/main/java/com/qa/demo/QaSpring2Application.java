146d4ff076d74c52a5820a4081d3ca8431b045e0
Andrew-McCall <amccall@qa.com>
2022-10-24 10:09:52 +0100

    Intial SpringBoot Starter

---
+++ src/main/java/com/qa/demo/QaSpring2Application.java
@@ -0,0 +1,13 @@
+package com.qa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaSpring2Application.class, args);
+	}
+
+}

bb6d4ef3c2b965426129d9341fc897a530a61c43
Andrew-McCall <amccall@qa.com>
2022-10-25 16:27:26 +0100

    Update and Delete

--- src/main/java/com/qa/runner/TrainerController.java
+++ src/main/java/com/qa/runner/TrainerController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +50,22 @@ public class TrainerController {
 			return trainers.size() - 1;
 		}
 		return -1;
+	}
+
+	@PutMapping("/update/{index}")
+	public Trainer update(@RequestBody Trainer trainer, @PathVariable("index") int index) {
+		if (index >= 0 && index < trainers.size()) {
+			return trainers.set(index, trainer);
+		}
+		return null;
+	}
+
+	@DeleteMapping("/delete/{index}")
+	public boolean deleteOne(@PathVariable("index") int index) {
+		if (index >= 0 && index < trainers.size()) {
+			return (trainers.remove(index) != null);
+		}
+		return false;
 	}
 
 }
